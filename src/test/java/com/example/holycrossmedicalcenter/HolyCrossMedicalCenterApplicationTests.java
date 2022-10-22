ba1d8dc29b9efb780b665770e880f13960c46ed3
HIDRI A <agthidri24@yahoo.com>
2022-10-22 17:15:21 -0500

    the first commit

---
+++ src/test/java/com/example/holycrossmedicalcenter/HolyCrossMedicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holycrossmedicalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyCrossMedicalCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
